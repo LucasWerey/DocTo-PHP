a27f5f0b19ddaf5538fc7c5e492e2b205d122d3a
Lenahtx <78757163+Lenahtx@users.noreply.github.com>
2022-05-24 12:48:36 +0200

    Co-authored-by: Clemence-ama <Clemence-ama@users.noreply.github.com>

--- octave.php
+++ octave.php
@@ -3,15 +3,11 @@
     $res=mysqli_query($id,"SELECT * from medecins");
     /*echo mysqli_num_rows($res)." enregistrements";
     echo "<hr />";*/
-    $lien = isset ($_POST['lien']) ? $_POST['lienitle'] : '';
     while($tab=mysqli_fetch_assoc($res)){
-        if ($lien == $tab['prenom'] + ' ' + $tab['nom']){
-
+        if ($tab['id']==2){
+            echo implode("<br>",$tab);
         }
-        echo implode("   ",$tab);
-        echo "<br/>";
     }
-
     mysqli_close($id);
     
 ?>
