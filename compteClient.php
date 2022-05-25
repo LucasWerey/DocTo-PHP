c8f8c3ead3387ce7bf8907ab5e3ccab8658b5790
Clemence-ama <78794459+Clemence-ama@users.noreply.github.com>
2022-05-25 14:09:32 +0200

    Co-authored-by: Lena HEURTAUX <lena.heurtaux@edu.ece.fr> Co-authored-by: Lucas WEREY <lucas.werey@edu.ece.fr>

--- compteClient.php
+++ compteClient.php
@@ -40,11 +40,15 @@
               <li class="nav-item active">
                 <a class="nav-link" href="verifcompte.php">Votre Compte <span class="sr-only">(current)</span> </a>
               </li>
+             
             </ul>
-            <a class="btn btn-outline-success my-2 my-sm-0" href="deconn.php">Déconnexion</a>
-            <form class="form-inline mt-2 mt-md-0">
+              <form class="form-inline mt-2 mt-md-0" style="padding-right:>
               <input class="form-control mr-sm-2 " type="text" placeholder="Votre recherche" aria-label="Search">
               <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Chercher</button>
+              </form>
+
+            <form class="form-inline mt-2 mt-md-0" style="margin-right:-150px;">
+            <a href="deconn.php"><img src="Images/logo.png" width="20%"></a>
             </form>
           </div>
         </nav>
