33368dcb904ec938067ce5205680bd62e3fe2b0a
Clemence-ama <78794459+Clemence-ama@users.noreply.github.com>
2022-05-26 17:13:22 +0200

    commit

--- search.php
+++ search.php
@@ -2,54 +2,48 @@
 
 <?php
 
-    //Référence à la page précédente
-    $referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'acceuil.php';
+//Référence à la page précédente
+$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : 'acceuil.php';
 
-    if (isset($_GET['terme'])){
-        $db = mysqli_connect('localhost', 'root', 'root','omnessante') or die('could not connect to database');
-        $terme = mysqli_real_escape_string($db,htmlspecialchars($_GET['terme']));
-        if($terme !== "")
-        {
-            $requete="SELECT * FROM medecins WHERE nom LIKE '".$terme."' or prenom LIKE '".$terme."' or spe LIKE '".$terme."' ";
-            $exec_requete = mysqli_query($db,$requete) or die(mysqli_error());
-            $total = mysqli_num_rows($exec_requete);
-            
-            if ($total > 0){
-                while($reponse = mysqli_fetch_array($exec_requete)){
-                    $spe=$reponse['spe'];
-                    $nom=$reponse['nom'];
-                    $prenom=$reponse['prenom'];
-            
-                    $terme = strtolower($terme);
-                    if ($terme==strtolower("Generaliste")||$terme==strtolower("Généraliste")){
+if (isset($_GET['terme'])) {
+    $db = mysqli_connect('localhost', 'root', 'root', 'omnessante') or die('could not connect to database');
+    $terme = mysqli_real_escape_string($db, htmlspecialchars($_GET['terme']));
+    if ($terme !== "") {
+        $requete = "SELECT * FROM medecins WHERE nom LIKE '" . $terme . "' or prenom LIKE '" . $terme . "' or spe LIKE '" . $terme . "' ";
+        $exec_requete = mysqli_query($db, $requete) or die(mysqli_error());
+        $total = mysqli_num_rows($exec_requete);
+
+        if ($total > 0) {
+            while ($reponse = mysqli_fetch_array($exec_requete)) {
+                $spe = $reponse['spe'];
+                $nom = $reponse['nom'];
+                $prenom = $reponse['prenom'];
+
+                $terme = strtolower($terme);
+                if ($terme == strtolower("Generaliste") || $terme == strtolower("Généraliste")) {
+                    header('Location: medgenerale.html');
+                } elseif (strtolower($spe) == $terme) {
+                    header("Location: " . $terme . ".php");
+                } elseif (strtolower($nom) == $terme || strtolower($prenom) == $terme) {
+                    $spe = strtolower($spe);
+                    if ($spe == strtolower("Generaliste") || $spe == strtolower("Généraliste")) {
                         header('Location: medgenerale.html');
-                    }
-                    elseif (strtolower($spe)==$terme){
-                        header("Location: ".$terme.".php");
-                    }elseif (strtolower($nom)==$terme || strtolower($prenom)==$terme){
-                        $spe=strtolower($spe);
-                        if ($spe==strtolower("Generaliste")||$spe==strtolower("Généraliste")){
-                            header('Location: medgenerale.html');
-                        }else{
-                            header("Location: ".$spe.".php");
-                        }
+                    } else {
+                        header("Location: " . $spe . ".php");
                     }
                 }
             }
-            else
-            {
-                //Retour à la page précédente
-                header("Location: ".$referer."?erreur=1"); // Terme non trouvé dans la table
-            }
-        }else
-        {
+        } else {
             //Retour à la page précédente
-           header("Location: ".$referer."?erreur=2"); // Terme vide
+            header("Location: " . $referer . "?erreur=1"); // Terme non trouvé dans la table
         }
-    }else
-    {
+    } else {
         //Retour à la page précédente
-        header("Location: ".$referer.".php?erreur=3"); //Le $terme n'existe pas
+        header("Location: " . $referer . "?erreur=2"); // Terme vide
     }
-    mysqli_close($db); // fermer la connexion
+} else {
+    //Retour à la page précédente
+    header("Location: " . $referer . ".php?erreur=3"); //Le $terme n'existe pas
+}
+mysqli_close($db); // fermer la connexion
 ?>
